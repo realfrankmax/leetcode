d0d72dcd0e845d18ac92518a0ac13f419d452a8a
realfrankmax <ma89long@gmail.com>
2023-08-23 20:29:39 -0700

    Updated find median of two sorted array with a cleaner solution

--- BinarySearch/FindMedianSortedArrays.java
+++ BinarySearch/FindMedianSortedArrays.java
@@ -5,58 +5,96 @@ package BinarySearch;
  */
 public class FindMedianSortedArrays {
     public static void main(String[] args) {}
-
+    int[] nums1;
+    int[] nums2;
     /**
      * @Time O(log(min(m, n)))
      * @Space O(1)
-     * @Method 
-     * A[0], A[1], ..., A[i-1]  |  A[i], A[i+1], ..., A[m-1] : i is [0, m]
-     * B[0], B[1], ..., B[j-1]  |  B[j], B[j+1], ..., B[n-1] : j is [0, n]
-     * 1. len(left_part) == len(right_part)
-     * 2. max(left_part) ≤ min(right_part)
-     * Then i + j == (m-i) + (n-j) + 1  ===> j = (m+n+1)/2 - i 
-     * (NOTE: if m+n is even, +1 no difference. if m+n is odd, +1 to make the median in the left part.
-     * B[j−1] ≤ A[i] and A[i−1] ≤ B[j]
      */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        int m = nums1.length;
+        int n = nums2.length;
+        int total = m + n;
+        if (total%2 == 0) {
+            return (binarySearch(0, m-1, 0, n-1, total/2)+binarySearch(0, m-1, 0, n-1, total/2-1)) / 2.0;
+        } else {
+            return (double) binarySearch(0, m-1, 0, n-1, total/2);
+        }
+    }
+    private int binarySearch(int left1, int right1, int left2, int right2, int k) {
+        if (left1 > right1) {
+            return this.nums2[k-left1];
+        }
+        if (left2 > right2) {
+            return this.nums1[k-left2];
+        }
+        int mid1 = (left1 + right1)/2;
+        int mid2 = (left2 + right2)/2;
+
+        if (mid1+mid2 < k) {
+            // The kth item must be in the larger half of combined arrays
+            if (this.nums1[mid1] > this.nums2[mid2]) {
+                // The kth item must not be in the smaller half of the array
+                // Smaller half of nums2 is the one.
+                return binarySearch(left1, right1, mid2+1, right2, k);
+            } else {
+                // Smaller half of nums1 is the one.
+                return binarySearch(mid1+1, right1, left2, right2, k);
+            }
+        } else {
+            // The kth item must be in the smaller half of combined arrays
+            if (this.nums1[mid1] > this.nums2[mid2]) {
+                return binarySearch(left1, mid1-1, left2, right2, k);
+            } else {
+                return binarySearch(left1, right1, left2, mid2-1, k);
+            }
+        }
+    }
+    /**
+     * Time O(m+n)
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        if (m > n) {
-            return findMedianSortedArrays(nums2, nums1);
+        int mid = (m+n+1)/2;
+        if ((m+n)%2 == 0) {
+            return (find(nums1, nums2, mid) + find(nums1, nums2, mid+1)) / 2.0;
+        } else {
+            return (double) find(nums1, nums2, mid);
         }
-        int left = 0;
-        int right = m;
-        int half = (m + n + 1)/2;
-        while (left <= right) {
-            int mid1 = left + (right - left)/2;
-            int mid2 = half - mid1;
-            if (mid1 > left && nums1[mid1-1] > nums2[mid2]) {
-                right = mid1 - 1;
-            } else if (mid1 < right && nums2[mid2-1] > nums1[mid1]) {
-                left = mid1 + 1;
-            } else {
-                int maxLeft = 0;
-                if (mid1 == 0) {
-                    maxLeft = nums2[mid2-1];
-                } else if (mid2 == 0) {
-                    maxLeft = nums1[mid1-1];
+    }
+    private int find(int[] nums1, int[] nums2, int c) {
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        while(count != c) {
+            count++;
+            if (i < nums1.length && j < nums2.length) {
+                if (nums1[i] < nums2[j]) {
+                    if (count == c) {
+                        return nums1[i];
+                    }
+                    i++;
                 } else {
-                    maxLeft = Math.max(nums1[mid1-1], nums2[mid2-1]);
+                    if (count == c) {
+                        return nums2[j];
+                    }
+                    j++;
                 }
-                if ((m+n)%2 != 0) {
-                    return maxLeft;
+            } else if (i < nums1.length) {
+                if (count == c) {
+                    return nums1[i];
                 }
-                int minRight = 0;
-                if (mid1 == m) {
-                    minRight = nums2[mid2];
-                } else if (mid2 == n) {
-                    minRight = nums1[mid1];
-                } else {
-                    minRight = Math.min(nums1[mid1], nums2[mid2]);
+                i++;
+            } else if (j < nums2.length) {
+                if (count == c) {
+                    return nums2[j];
                 }
-                return (maxLeft + minRight)/2.0;
+                j++;
             }
         }
-        return 0.0;
+        return -1;
     }
+    */
 }
